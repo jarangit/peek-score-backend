8bd86a12bff0edb07ff99a0af4abf54149b897b0
jarangit <52028847+jarangit@users.noreply.github.com>
2025-03-09 02:19:11 +0700

    enable CORS support in the main application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // ✅ อนุญาตทุก Origin
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
