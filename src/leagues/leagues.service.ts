116a5f392706cc8adecbc669ab76076768f7a198
jarangit <jaran.dch@gmail.com>
2025-03-24 19:58:19 +0700

    remove commented-out league filtering logic in getLeagues method

--- src/leagues/leagues.service.ts
+++ src/leagues/leagues.service.ts
@@ -6,13 +6,13 @@ export class LeaguesService {
   async getLeagues() {
     try {
       const response: any = await firstValueFrom(of(mockDataLeagues));
-      const leagueIds = [39, 140, 135, 78, 61, 2, 3, 1, 45, 143, 137];
-      const leagues = mockDataLeagues.response.filter((item: any) =>
-        leagueIds.includes(item.league.id),
-      );
+      // const leagueIds = [39, 140, 135, 78, 61, 2, 3, 1, 45, 143, 137];
+      // const leagues = mockDataLeagues.response.filter((item: any) =>
+      //   leagueIds.includes(item.league.id),
+      // );
       return {
         ...response,
-        response: leagues,
+        // response: leagues,
       };
     } catch (error) {
       throw new Error('Failed to fetch leagues');
